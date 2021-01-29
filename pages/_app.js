f06698cf1aa2d4c6574971dd5a0a4744f11dfba3
Bruno Gerbasi <bruno_gerbasi@hotmail.com>
2021-01-29 23:40:54 +0000

    og image

--- pages/_app.js
+++ pages/_app.js
@@ -38,9 +38,7 @@ export default function App({ Component, pageProps }) {
         <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet" />
       </Head>
       <Head>
-        <meta property="og:image" content="https://mantra.pt/Themes/NobleTheme/Content/assets/alura/bg-hp.jpg" />
-        <meta property="og:image:width" content="600" />
-        <meta property="og:image:height" content="315" />
+        <meta property="og:image" content="https://mantra.pt/Themes/NobleTheme/Content/assets/alura/bgog.jpg" />
       </Head>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
