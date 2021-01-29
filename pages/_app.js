d1c2557ff89e99a5d3e4f7c8f391bcbf958e7d4f
Bruno Gerbasi <bruno_gerbasi@hotmail.com>
2021-01-29 23:37:18 +0000

    head meta tags

--- pages/_app.js
+++ pages/_app.js
@@ -36,6 +36,8 @@ export default function App({ Component, pageProps }) {
       <Head>
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet" />
+      </Head>
+      <Head>
         <meta property="og:image" content="https://mantra.pt/Themes/NobleTheme/Content/assets/alura/bg-hp.jpg" />
         <meta property="og:image:width" content="600" />
         <meta property="og:image:height" content="315" />
